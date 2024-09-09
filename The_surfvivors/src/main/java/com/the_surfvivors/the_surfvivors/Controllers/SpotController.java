e7761606300cebeaf9af7fff7c5c20c86b562dde
Noemie-Gyc <goyec.noemie@gmail.com>
2024-09-09 16:32:32 +0200

    création du CRUD dans Controller

--- The_surfvivors/src/main/java/com/the_surfvivors/the_surfvivors/Controllers/SpotController.java
+++ The_surfvivors/src/main/java/com/the_surfvivors/the_surfvivors/Controllers/SpotController.java
@@ -1,11 +1,10 @@
 package com.the_surfvivors.the_surfvivors.Controllers;
 
+import com.the_surfvivors.the_surfvivors.Exceptions.ResourceNotFoundException;
 import com.the_surfvivors.the_surfvivors.Models.Spot;
 import com.the_surfvivors.the_surfvivors.Repository.SpotRepo;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,9 +20,41 @@ public class SpotController {
         this.spotRepo = spotRepo;
     }
 
-    @GetMapping("/Spot")
+    @GetMapping("/spots")
     public List<Spot> getAllSpots(){
+        return spotRepo.findAll();
         
     }
 
+    @GetMapping("/spots/{id}")
+    public Spot getSpotById(@PathVariable(value = "id")Long SpotID) throws ResourceNotFoundException {
+        return spotRepo.findById(SpotID)
+                .orElseThrow(() -> new ResourceNotFoundException("Spot avec ID : " + SpotID + " est inexistant"));
+    }
+
+    @DeleteMapping("/spots/{id}")
+    public void deleteSpot(@PathVariable(value = "id")Long SpotID) throws ResourceNotFoundException {
+        Spot spot = spotRepo.findById(SpotID)
+                .orElseThrow(() -> new ResourceNotFoundException("Spot avec ID : " + SpotID + " est inexistant"));
+
+        spotRepo.delete(spot);
+    }
+
+@PutMapping("/spots/{id}")
+    public Spot updateSpot(@PathVariable(value = "id")Long SpotID,
+                           @Validated @RequestBody Spot spotDetails) throws ResourceNotFoundException {
+        Spot spot = spotRepo.findById(SpotID)
+                .orElseThrow(() -> new ResourceNotFoundException("Spot avec ID : " + SpotID + " est inexistant"));
+             spot.setFields(spotDetails.getFields());
+             spot.setDifficulty_level(spotDetails.getDifficulty_level());
+             spot.setDestination(spotDetails.getDestination());
+             spot.setPhotos(spotDetails.getPhotos());
+
+             return spotRepo.save(spot);
+}
+
+@PostMapping("/spots")
+    public Spot createSpot(@Validated @RequestBody Spot spot){
+        return spotRepo.save(spot);
+}
 }
